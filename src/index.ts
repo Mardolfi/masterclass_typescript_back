ebbfaa1e4e310957fca3efd0625c48d72f89b324
mardolfi <mardolfi1607@gmail.com>
2023-01-17 13:51:42 -0300

    finished

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import routes from './routes';
+import cors from 'cors'
 
 const app = express();
-const port = 3000
+const port = 3333
 
+app.use(cors())
 app.use(routes);
 
 app.listen(port, () => console.log(`Listening on ${port}`))
